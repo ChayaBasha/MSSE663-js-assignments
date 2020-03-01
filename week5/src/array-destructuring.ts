96518e12e0d726bbfb514624b4178c3122015ff7
ChayaBasha <johanna.blumenthal@gmail.com>
2020-03-01 14:35:21 -0700

    changed the [b,a] confusion and destructured #3 before making statement as comments suggested

--- week5/src/array-destructuring.ts
+++ week5/src/array-destructuring.ts
@@ -7,27 +7,25 @@ export const fruits = ['apple', 'banana'];
 
 // deconstruct here
 
-var [a,b] = fruits;
-console.log(a);
-console.log(b);
+export var [b, a] = fruits;
+
 
 // Assign variables using ES6 so that we get (note, you cannot just make a completely new array):
 // console.log(a); // 'banana';
 // console.log(b); // 'apple';
 
-export var array1 = [a,b] = [b,a];
-
 // #2 Variables and Rest
 export const food = ['apple', 'banana', 'chocolate', 'pears', 'oats', 'pizza'];
 
 // deconstruct here
-var [a,b, ...others] = food;
-export var food2 = [a,b,others];
+var [c,d, ...others] = food;
+export var food2 = [c,d,others];
 
-console.log(a); // 'apple'
-console.log(b); // 'banana'
+console.log(c); // 'apple'
+console.log(d); // 'banana'
 console.log(others); // ['chocolate, 'pears', 'oats', 'pizza']
 
 // #3 Array: Contruct a statement
 const words = ['hello', 'taylor'];
-export const statement = "'" +words.join(", ")+"!'";// 'hello, taylor!'
+export const [salutation, person] = words;
+export const statement = "'" + salutation +',' + ' ' + person + '!' + "'";
