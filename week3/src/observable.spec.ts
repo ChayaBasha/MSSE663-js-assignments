2fd95c15dc833fccfb5c7f45975e747250d4e4fd
ChayaBasha <59859876+ChayaBasha@users.noreply.github.com>
2020-03-01 13:13:09 -0700

    Resubmit with fixed test for observable (#6)

--- week3/src/observable.spec.ts
+++ week3/src/observable.spec.ts
@@ -5,7 +5,8 @@ import {heroinesWithReference$} from './observable';
 
 describe('test for observable.ts', () => {
     it('should add imanu to the end of each Heroine name', () => {
-        expect(heroinesWithReference$.subscribe(console.log)).to.eql(["Sarah Imanu","Rivkah Imanu","Rachel Imanu","Leah Imanu" ]);
+        const expected = ['Sarah Imanu', 'Rivka Imanu', 'Rachel Imanu', 'Leah Imanu'];
+        heroinesWithReference$.subscribe(heroine => { expect(heroine).to.equal(expected);
+    });
     });
 });
-
