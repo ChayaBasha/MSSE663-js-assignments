b315e9c9cf9e98807757a474b6f04fd0ba41a364
ChayaBasha <johanna.blumenthal@gmail.com>
2020-03-01 13:10:06 -0700

    Resubmit with fixed test for observable

--- week3/src/observable.ts
+++ week3/src/observable.ts
@@ -2,7 +2,7 @@
 import {of} from 'rxjs';
 import {map} from 'rxjs/operators';
 
-export const heroine$ = of("Sarah","Rivkah", "Rachel", "Leah");
+const heroine$ = of("Sarah","Rivkah", "Rachel", "Leah");
 
 export const heroinesWithReference$ = heroine$.pipe(map((heroine: string, index: number) => heroine + " " + "Imanu" ));
 
