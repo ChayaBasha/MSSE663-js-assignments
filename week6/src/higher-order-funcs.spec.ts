5a6f711b0d8a27a01e8c2bb8cccca3fb291cbbe6
ChayaBasha <johanna.blumenthal@gmail.com>
2020-03-02 13:19:35 -0700

    completed assignment added higher order functions piece

--- week6/src/higher-order-funcs.spec.ts
+++ week6/src/higher-order-funcs.spec.ts
@@ -1,8 +1,17 @@
 import { expect } from 'chai';
 import 'mocha';
+import { thingsAsObjects, iFilterThings } from './higher-order-funcs';
 
 describe('Higher Order Function tests', () => {
   it("#1 should produce the expected result `[{ id: 1, name 'thing' }, { id: 2, name 'thing' }]` after hoisting", () => {});
-
+    const expected = [
+      {id: 1, name:'thing1'},
+      {id: 2, name:'thing2'}
+    ]
+    expect (thingsAsObjects).to.eql(expected);
   it('#2 should return a single thing from array of things objects after hoisting and currying', () => {});
+    const expected2 = [
+      {id: 1, name: 'thing1'}
+    ]
+    expect (iFilterThings('thing1')).to.eql(expected2);
 });
--- week6/src/higher-order-funcs.ts
+++ week6/src/higher-order-funcs.ts
@@ -11,17 +11,22 @@
 const things = ['thing1', 'thing2'];
 
 
-const makeObject = (value:string, index: number) => ({id:index, name:value}); 
-const thingsAsObjects = things.map(makeObject);
+function makeObject (value:string, index: number) {
+    return ({id:index+1, name:value});
+}; 
+export const thingsAsObjects = things.map(makeObject);
 
 /**
  * #2 Higher order then curry.
  *
  * - Write a function that accepts a key as a string.
-
- * 
- * - Filter the results from 1 to return a single thing object.
- * 
- * 
+ * - Filter the results from #1 above to return a single thing object.
  * - Hoist that filter callback and curry all functions.
  */
+interface Thing {
+    id: number;
+    name: string; 
+};
+ const iTakeStrings = (key:string) => (thing:Thing) => thing.name === key;
+ 
+export const iFilterThings = (key: string) => thingsAsObjects.filter(iTakeStrings(key));
